c3435d846dbeaca2aec512279a02ba80054fea57
ser-gent <ser-gent.dev@outlook.com>
2021-05-10 11:06:36 +0200

    Add bug report link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="fixed-bottom mt-4 mb-2 ms-4">
-    &copy-2021 - Sirona interface - version : {{ env('APP_VERSION') }}
+    &copy-2021 - Sirona - version : {{ env('APP_VERSION') }} - <a href="https://github.com/ser-gent/sirona/issues">Reporter un bug</a>
 </div>
